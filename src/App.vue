83e9c661ea097fafb840a2741ab4d562c766a9e0
Thomas-Basadonne <thommybasa@gmail.com>
2023-12-27 16:16:16 +0100

    end coming soon page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
-<script setup></script>
+<script setup>
+import WorkInProgress from "./components/WorkInProgress.vue";
+</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <WorkInProgress />
 </template>
 
 <style scoped></style>
