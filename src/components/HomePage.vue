3ed7378f15c9f130ab3ee51a4566b00551e3ab99
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-02 16:23:19 +0100

    main views init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+import JumboApp from "./_partials/JumboApp.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: {
+    JumboApp,
+  },
+};
+</script>
+
+<template>
+  <JumboApp />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,22 @@
+<script>
+// Esempio di importazione corretta
+import CardApp from "./_partials/CardApp.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello portfolio",
+    };
+  },
+
+  components: {
+    CardApp,
+  },
+};
+</script>
+
+<template>
+  <CardApp />
+</template>
+
+<style lang="scss" scoped></style>
