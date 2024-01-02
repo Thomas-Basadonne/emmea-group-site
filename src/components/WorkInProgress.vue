41a08b017ebc7d29b547d3d68dd890df816e6876
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-02 02:02:24 +0100

    init card component

--- src/components/WorkInProgress.vue
+++ src/components/WorkInProgress.vue
@@ -1,6 +1,7 @@
 <script>
 import HeaderApp from "./_partials/HeaderApp.vue";
 import JumboApp from "./_partials/JumboApp.vue";
+import CardApp from "./_partials/CardApp.vue";
 import FooterApp from "./_partials/FooterApp.vue";
 
 export default {
@@ -14,6 +15,7 @@ export default {
     HeaderApp,
     JumboApp,
     FooterApp,
+    CardApp,
   },
 };
 </script>
@@ -21,6 +23,7 @@ export default {
 <template>
   <HeaderApp />
   <JumboApp />
+  <CardApp />
 
   <div class="bg-gray-200 font-sans leading-normal tracking-normal">
     <!-- Hero section -->
