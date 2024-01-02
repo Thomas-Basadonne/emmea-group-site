5723d88e391c6859c25ff019a8c21789923193bd
Thomas-Basadonne <thommybasa@gmail.com>
2024-01-02 15:26:42 +0100

    edit style card

--- src/components/_partials/CardApp.vue
+++ src/components/_partials/CardApp.vue
@@ -17,16 +17,18 @@ export default {
 <template>
   <div class="flex justify-center gap-20 my-28">
     <div
-      class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900 text-white"
+      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white hover:shadow-[16px_-15px_28px_4px_#38BDF8] transition duration-300"
     >
+      <!-- IMMAGINE -->
       <img
-        class="w-full"
-        src="../../assets/img/favicon.png"
+        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+        src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="Sunset in the mountains"
       />
+      <!-- TESTO -->
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-white text-base">
+        <div class="my-4 font-bold text-3xl">The Coldest Sunset</div>
+        <p class="text-white">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
@@ -34,25 +36,26 @@ export default {
       </div>
       <div class="px-6 pt-4 pb-2">
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#photography</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#travel</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#winter</span
         >
       </div>
     </div>
+
     <div
       class="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-gray-900 text-white"
     >
       <img
-        class="w-full"
-        src="../../assets/img/favicon.png"
+        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+        src="https://images.pexels.com/photos/417458/pexels-photo-417458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="Sunset in the mountains"
       />
       <div class="px-6 py-4">
@@ -65,25 +68,26 @@ export default {
       </div>
       <div class="px-6 pt-4 pb-2">
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#photography</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#travel</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#winter</span
         >
       </div>
     </div>
+
     <div
-      class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900 text-white"
+      class="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-gray-900 text-white"
     >
       <img
-        class="w-full"
-        src="../../assets/img/favicon.png"
+        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+        src="https://images.pexels.com/photos/2526105/pexels-photo-2526105.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="Sunset in the mountains"
       />
       <div class="px-6 py-4">
@@ -96,15 +100,15 @@ export default {
       </div>
       <div class="px-6 pt-4 pb-2">
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#photography</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#travel</span
         >
         <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          class="inline-block bg-sky-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2"
           >#winter</span
         >
       </div>
