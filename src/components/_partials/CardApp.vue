81181b63c372021c71bb6cd9d33329e1deb17b0e
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-03 18:51:25 +0100

    add daisy ui

--- src/components/_partials/CardApp.vue
+++ src/components/_partials/CardApp.vue
@@ -1,5 +1,6 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import { onMounted } from "vue";
+import { themeChange } from "theme-change";
 
 export default {
   data() {
@@ -11,21 +12,25 @@ export default {
   // components: {
   //   MyComponent,
   // },
+
+  
 };
 </script>
 
 <template>
+  
+
   <div class="flex justify-center flex-wrap gap-20 my-28">
-    <div
-      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white hover:shadow-[16px_-15px_28px_4px_#38BDF8] transition duration-300"
+    <!-- <div
+      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white shadow-card transition duration-300"
     >
-      <!-- IMMAGINE -->
+     
       <img
         class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
         src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="Sunset in the mountains"
       />
-      <!-- TESTO -->
+     
       <div class="px-6 py-4">
         <div class="my-4 font-bold text-3xl">The Coldest Sunset</div>
         <p class="text-white">
@@ -38,56 +43,58 @@ export default {
         <span class="label">#photography</span>
         <span class="label">#travel</span>
         <span class="label">#winter</span>
+      </div>
+    </div> -->
+
+    <div class="card w-96 bg-base-100 shadow-card">
+      <figure>
+        <img
+          class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="Sunset in the mountains"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
       </div>
     </div>
-
-    <div
-      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white hover:shadow-[16px_-15px_28px_4px_#38BDF8] transition duration-300"
-    >
-      <!-- IMMAGINE -->
-      <img
-        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-        src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Sunset in the mountains"
-      />
-      <!-- TESTO -->
-      <div class="px-6 py-4">
-        <div class="my-4 font-bold text-3xl">The Coldest Sunset</div>
-        <p class="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="label">#photography</span>
-        <span class="label">#travel</span>
-        <span class="label">#winter</span>
+    <div class="card w-96 bg-base-100 shadow-card">
+      <figure>
+        <img
+          class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="Sunset in the mountains"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
       </div>
     </div>
-
-    <div
-      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white hover:shadow-[16px_-15px_28px_4px_#38BDF8] transition duration-300"
-    >
-      <!-- IMMAGINE -->
-      <img
-        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-        src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Sunset in the mountains"
-      />
-      <!-- TESTO -->
-      <div class="px-6 py-4">
-        <div class="my-4 font-bold text-3xl">The Coldest Sunset</div>
-        <p class="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="label">#photography</span>
-        <span class="label">#travel</span>
-        <span class="label">#winter</span>
+    <div class="card w-96 bg-base-100 shadow-card">
+      <figure>
+        <img
+          class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
+          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="Sunset in the mountains"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
       </div>
     </div>
   </div>
