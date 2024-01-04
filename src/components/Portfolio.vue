4190fa8e760d559ea06eb2c90d7de95ba4d6e21b
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-04 17:55:15 +0100

    edit portfolio + add filter & smart v-for

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -16,6 +16,16 @@ export default {
 </script>
 
 <template>
+  <section class="my-10 w-10/12 md:w-8/12 lg:w-7/12 mx-auto">
+    <h1 class="text-center font-bold text-6xl">Il nostro Portfolio</h1>
+    <p class="text-lg mt-6 text-center">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis ea
+      obcaecati quis excepturi sint? Natus odio vitae minima error cumque
+      eveniet voluptatibus laborum quidem corporis hic soluta enim, quo
+      delectus.
+    </p>
+  </section>
+  <div class="divider mx-8 md:mx-10 lg:mx-12"></div>
   <CardApp />
 </template>
 
--- src/components/_partials/CardApp.vue
+++ src/components/_partials/CardApp.vue
@@ -3,90 +3,104 @@ export default {
   data() {
     return {
       title: "Hello world",
+      selectedLabel: "",
+      projects: [
+        {
+          imageSrc:
+            "https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=600",
+          title: "Shoes!",
+          description: "If a dog chews shoes, whose shoes does he choose?",
+          labels: ["Fashion", "Products"],
+        },
+        {
+          imageSrc:
+            "https://images.pexels.com/photos/14866387/pexels-photo-14866387.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+          title: "Nike",
+          description: "If a dog chews shoes, whose shoes does he choose?",
+          labels: ["Fashion", "Products"],
+        },
+        {
+          imageSrc:
+            "https://images.pexels.com/photos/1120162/pexels-photo-1120162.jpeg?auto=compress&cs=tinysrgb&w=600",
+          title: "jordan",
+          description: "If a dog chews shoes, whose shoes does he choose?",
+          labels: ["Fashion", "Products"],
+        },
+        {
+          imageSrc:
+            "https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&w=600",
+          title: "Boh!",
+          description: "If a dog chews shoes, whose shoes does he choose?",
+          labels: ["Fashion"],
+        },
+      ],
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  computed: {
+    // Ottenere tutte le labels univoche da tutti i progetti
+    allLabels() {
+      const labelsSet = new Set();
+      this.projects.forEach((project) => {
+        project.labels.forEach((label) => {
+          labelsSet.add(label);
+        });
+      });
+      return Array.from(labelsSet);
+    },
+    // Filtrare i progetti in base alla label selezionata
+    filteredProjects() {
+      if (!this.selectedLabel) {
+        return this.projects;
+      }
+      return this.projects.filter((project) =>
+        project.labels.includes(this.selectedLabel)
+      );
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="flex justify-center flex-wrap gap-20 my-28">
-    <!-- <div
-      class="max-w-sm rounded-3xl overflow-hidden bg-gray-900 text-white shadow-card transition duration-300"
+  <div class="flex justify-center sm:justify-end mx-8 md:mx-10 lg:mx-12">
+    <select
+      v-model="selectedLabel"
+      class="select select-bordered w-full max-w-xs"
     >
-     
-      <img
-        class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-        src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Sunset in the mountains"
-      />
-     
-      <div class="px-6 py-4">
-        <div class="my-4 font-bold text-3xl">The Coldest Sunset</div>
-        <p class="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="label">#photography</span>
-        <span class="label">#travel</span>
-        <span class="label">#winter</span>
-      </div>
-    </div> -->
-
-    <div class="card w-96 bg-base-100 shadow-card">
+      <option disabled value>Filtra progetti</option>
+      <option value="">Mostra tutti i progetti</option>
+      <option v-for="label in allLabels" :key="label" :value="label">
+        {{ label }}
+      </option>
+    </select>
+  </div>
+  <!-- CARDS GROUP -->
+  <div
+    class="flex justify-center flex-wrap gap-16 my-20 mx-8 md:mx-10 lg:mx-12"
+  >
+    <div
+      v-for="(project, index) in filteredProjects"
+      :key="index"
+      class="card w-80 bg-base-100 shadow-card"
+    >
       <figure>
         <img
           class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Sunset in the mountains"
+          :src="project.imageSrc"
+          :alt="`Image for ${project.title}`"
         />
       </figure>
       <div class="card-body">
-        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <div class="badge badge-outline">Fashion</div>
-          <div class="badge badge-outline">Products</div>
-        </div>
-      </div>
-    </div>
-    <div class="card w-96 bg-base-100 shadow-card">
-      <figure>
-        <img
-          class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Sunset in the mountains"
-        />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <div class="badge badge-outline">Fashion</div>
-          <div class="badge badge-outline">Products</div>
-        </div>
-      </div>
-    </div>
-    <div class="card w-96 bg-base-100 shadow-card">
-      <figure>
-        <img
-          class="w-full h-80 hover:scale-110 transition duration-300 object-cover"
-          src="https://images.pexels.com/photos/3661650/pexels-photo-3661650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Sunset in the mountains"
-        />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title font-bold text-3xl">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <div class="badge badge-outline">Fashion</div>
-          <div class="badge badge-outline">Products</div>
+        <h2 class="card-title font-bold text-3xl">{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <div class="card-actions justify-end mt-3">
+          <div
+            v-for="(label, labelIndex) in project.labels"
+            :key="labelIndex"
+            class="badge badge-outline"
+          >
+            {{ label }}
+          </div>
         </div>
       </div>
     </div>
