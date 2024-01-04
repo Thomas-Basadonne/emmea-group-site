33fc63108f8539412f6be1637989e37b0a2f560d
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-04 19:47:24 +0100

    form + validazione

--- src/components/_partials/FormApp.vue
+++ src/components/_partials/FormApp.vue
@@ -1,74 +1,106 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
     return {
       title: "Form",
+      formData: {
+        name: "",
+        email: "",
+        message: "",
+      },
     };
   },
+  computed: {
+    validateName() {
+      return this.formData.name.trim().length >= 4;
+    },
 
-  // components: {
-  //   MyComponent,
-  // },
+    validateEmail() {
+      // Logica di convalida dell'email
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return this.formData.email.match(emailRegex);
+    },
+    validateMessage() {
+      return this.formData.message.trim() !== "";
+    },
+  },
 };
 </script>
 
 <template>
-  <form>
-    <!-- Nome -->
-    <div class="mb-4">
-      <label for="name" class="block text-sm font-medium text-gray-600"
-        >Name</label
-      >
-      <input
-        type="text"
-        id="name"
-        name="name"
-        class="mt-1 p-2 w-full border rounded-md"
-        placeholder="John Doe"
-        required
-      />
-    </div>
-
-    <!-- Email -->
-    <div class="mb-4">
-      <label for="email" class="block text-sm font-medium text-gray-600"
-        >Email</label
-      >
-      <input
-        type="email"
-        id="email"
-        name="email"
-        class="mt-1 p-2 w-full border rounded-md"
-        placeholder="john@example.com"
-        required
-      />
-    </div>
-
-    <!-- Messaggio -->
-    <div class="mb-6">
-      <label for="message" class="block text-sm font-medium text-gray-600"
-        >Message</label
-      >
-      <textarea
-        id="message"
-        name="message"
-        rows="4"
-        class="mt-1 p-2 w-full border rounded-md"
-        placeholder="Write your message here..."
-        required
-      ></textarea>
-    </div>
-
-    <!-- Pulsante Invia -->
-    <button
-      type="submit"
-      class="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+  <div class="container mx-auto my-10">
+    <div
+      class="max-w-md mx-auto bg-white dark:bg-dark-black p-8 shadow-lg rounded-md"
     >
-      Send
-    </button>
-  </form>
+      <h2 class="text-2xl font-semibold mb-6 text-center">
+        Manda un messaggio!
+      </h2>
+
+      <!-- Nome -->
+      <div class="mb-4">
+        <label for="name" class="block text-dark-text text-sm font-bold mb-2"
+          >Nome</label
+        >
+        <input
+          type="text"
+          id="name"
+          name="name"
+          v-model="formData.name"
+          placeholder="Inserisci il tuo nome"
+          class="w-full bg-dark-primary border p-2 rounded-md"
+        />
+        <p v-if="!validateName" class="text-red-500 text-sm mt-1">
+          Il nome deve contenere almeno 4 caratteri.
+        </p>
+      </div>
+
+      <!-- Email -->
+      <div class="mb-4">
+        <label for="email" class="block text-dark-text text-sm font-bold mb-2"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          name="email"
+          v-model="formData.email"
+          placeholder="Inserisci la tua email"
+          class="w-full bg-dark-primary border p-2 rounded-md"
+        />
+        <p v-if="!validateEmail" class="text-red-500 text-sm mt-1">
+          Inserisci un'email valida.
+        </p>
+      </div>
+
+      <!-- Messaggio -->
+      <div class="mb-6">
+        <label for="message" class="block text-dark-text text-sm font-bold mb-2"
+          >Messaggio</label
+        >
+        <textarea
+          id="message"
+          name="message"
+          v-model="formData.message"
+          placeholder="Scrivi il tuo messaggio"
+          rows="4"
+          class="w-full bg-dark-primary border p-2 rounded-md"
+        ></textarea>
+        <p v-if="!validateMessage" class="text-red-500 text-sm mt-1">
+          Il messaggio non può essere vuoto.
+        </p>
+      </div>
+
+      <!-- Pulsante Invia -->
+      <div class="flex justify-end">
+        <button
+          @click="submitForm"
+          class="bg-dark-primary text-white px-4 py-2 rounded-md hover:bg-dark-red transition duration-300"
+        >
+          Invia
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
